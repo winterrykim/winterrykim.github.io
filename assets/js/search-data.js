5584203b96a941ebaefa3f3907aef75da7eaa81d
winterrykim <terry.kim@berkeley.edu>
2025-06-01 15:35:21 +0000

    Deploying to gh-pages from @ winterrykim/winterrykim.github.io@059c0b135ef5bdf0e5e130fa0e1e0524fd026cfa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,15 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-meaning-of-life",
+          title: "meaning of life",
+          description: "List of my efforts to figure out my meaning of life.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/big_life_moments/";
+          },
+        },{id: "nav-ai4science-blog",
+          title: "ai4Science blog",
           description: "",
           section: "Navigation",
           handler: () => {
@@ -23,20 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -44,19 +37,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+        },{id: "nav-teaching-and-edtech",
+          title: "teaching and edtech",
+          description: "List of involvement in teaching and education.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
@@ -64,13 +50,6 @@ ninja.data = [{
               section: "Dropdown",
               handler: () => {
                 window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
               },
             },{id: "post-a-post-with-plotly-js",
         
