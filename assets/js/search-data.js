f7f99ebf5fdc3b26e7b62256419d33b04c1684f5
winterrykim <terry.kim@berkeley.edu>
2025-06-01 15:59:12 +0000

    Deploying to gh-pages from @ winterrykim/winterrykim.github.io@afb68973e6b2093b02b8a8674a19198e8b784159 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching-and-edtech",
           title: "teaching and edtech",
           description: "List of involvement in teaching and education.",
